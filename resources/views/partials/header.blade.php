367137017f96b150fa02495fedf0cad928b60781
radfarelmira <radfar.elmira@gmail.com>
2022-02-09 15:34:23 +0100

    include header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,75 @@
 <header>
-    HEADER
+    <div class="header-top">
+        <div class="container">
+            <span>dc power visa</span>
+            <span>additional dc sites</span>
+        </div>
+    </div>
+
+    <div class="header-bottom">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('images/dc-logo.png')}}" alt="DC logo">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a class="active" href="#">
+                            characters 
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            movies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            tv
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            games
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            collectibles
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            videos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                          fans  
+                        </a>  
+                    </li>
+                    <li>
+                        <a href="#">
+                            news
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                           shop 
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                Search
+            </div>
+        </div>
+    </div>
 </header>
